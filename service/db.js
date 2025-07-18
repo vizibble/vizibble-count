63e2efdcad3509ffff34eb86fe37ff50f2bb0034
Saksham-12-03 <saksham@vizibble.in>
2025-07-18 20:45:00 +0530

    Fix Client Issue

--- service/db.js
+++ service/db.js
@@ -1,7 +1,5 @@
 const Client = require("pg").Client;
-require("dotenv").config()
-
-const { Client } = require("pg");
+require("dotenv").config();
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
